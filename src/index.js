7ac2a575b78e1b85ea6444dd15a20d3404e678f3
Dave Lockhart <dave.lockhart@d2l.com>
2015-06-17 15:26:27 -0400

    completely replacing Q with ES6 promises and the babel runtime

--- src/index.js
+++ src/index.js
@@ -1,27 +1,18 @@
 import Host from './host';
 import Client from './client';
-var Q = require('q');
 
 export function createHost(data) {
-
-	var deferred = Q.defer();
-
-	var host = new Host(data.id, data.src, function() {
-		deferred.resolve(host);
+	return new Promise((resolve, reject) => {
+		var host = new Host(data.id, data.src, function() {
+			resolve(host);
+		});
 	});
-
-	return deferred.promise;
-
 }
 
 export function createClient() {
-
-	var deferred = Q.defer();
-
-	var client = new Client(function(c) {
-		deferred.resolve(c);
+	return new Promise((resolve, reject) => {
+		var client = new Client(function(c) {
+			resolve(c);
+		});
 	});
-
-	return deferred.promise;
-
 }
