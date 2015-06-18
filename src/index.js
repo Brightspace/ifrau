9af95e29ec8a9f8e188ba1d2f05dd5a1e3374ee6
Dave Lockhart <dave.lockhart@d2l.com>
2015-06-18 14:01:44 -0400

    exporting both classes instead of factory methods

--- src/index.js
+++ src/index.js
@@ -1,18 +1,2 @@
-import Host from './host';
-import Client from './client';
-
-export function createHost(data) {
-	return new Promise((resolve, reject) => {
-		var host = new Host(data.id, data.src, function() {
-			resolve(host);
-		});
-	});
-}
-
-export function createClient() {
-	return new Promise((resolve, reject) => {
-		var client = new Client(function(c) {
-			resolve(c);
-		});
-	});
-}
+export {default as Host} from './host';
+export {default as Client} from './client';
