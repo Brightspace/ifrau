d742e9b395a68b5ef2ff8b0ffd66b152a3a0aa34
Dave Lockhart <dave.lockhart@d2l.com>
2016-02-02 11:55:45 -0500

    including dyslexic and visual redesign flags in font information and applying it on the client when VR flag is on

--- test/plugins/sync-font.js
+++ test/plugins/sync-font.js
@@ -20,9 +20,24 @@ MockHost.prototype.onRequest = function() {};
 
 describe('sync-font', () => {
 
+	var fontFamily = 'comic sans', classListAdd;
+
 	beforeEach(() => {
+		classListAdd = sinon.spy();
 		global.document = {
+			documentElement: {
+				style: {
+					fontSize: '1pt'
+				}
+			},
 			body: {
+				classList: {
+					add: classListAdd,
+					contains: function(className) {
+						return global.document.body.className.indexOf(className) > -1;
+					}
+				},
+				className: '',
 				style: {
 					fontFamily: 'verdana',
 					fontSize: '12px'
@@ -32,7 +47,7 @@ describe('sync-font', () => {
 		global.window = {
 			getComputedStyle: function() {
 				return {
-					fontFamily: 'comic sans',
+					fontFamily: fontFamily,
 					fontSize: '20pt'
 				};
 			}
@@ -41,13 +56,19 @@ describe('sync-font', () => {
 
 	describe('client', () => {
 
-		var client, request;
+		var client, request, response;
 
 		beforeEach(() => {
+			response = {
+				dyslexic: false,
+				family: 'foo',
+				size: 'bar',
+				visualRedesign: false
+			};
 			client = new MockClient();
 			request = sinon.stub(client, 'request').returns(
 				new Promise((resolve) => {
-					resolve({family: 'foo', size: 'bar'});
+					resolve(response);
 				})
 			);
 		});
@@ -61,10 +82,38 @@ describe('sync-font', () => {
 			request.should.have.been.calledWith('font');
 		});
 
-		it('should apply result to body', (done) => {
+		it('should apply result to body when redesign flag is off', (done) => {
 			clientSyncFont(client).then(() => {
 				expect(document.body.style.fontFamily).to.equal('foo');
 				expect(document.body.style.fontSize).to.equal('bar');
+				expect(document.documentElement.style.fontSize).to.equal('1pt');
+				done();
+			});
+		});
+
+		it('should apply only font size to HTML element when redesign flag is on', (done) => {
+			response.visualRedesign = true;
+			clientSyncFont(client).then(() => {
+				expect(document.body.style.fontFamily).to.equal('verdana');
+				expect(document.body.style.fontSize).to.equal('12px');
+				expect(document.documentElement.style.fontSize).to.equal('bar');
+				done();
+			});
+		});
+
+		it('should ignore dyslexic when flag is off', (done) => {
+			response.dyslexic = true;
+			clientSyncFont(client).then(() => {
+				classListAdd.should.have.not.been.called;
+				done();
+			});
+		});
+
+		it('should add dyslexic class when flag is on', (done) => {
+			response.visualRedesign = true;
+			response.dyslexic = true;
+			clientSyncFont(client).then(() => {
+				classListAdd.should.have.been.calledWith('vui-dyslexic');
 				done();
 			});
 		});
@@ -89,10 +138,51 @@ describe('sync-font', () => {
 			onRequest.should.have.been.calledWith('font');
 		});
 
-		it('should return computed style', () => {
+		it('should return computed font family and size', () => {
 			hostSyncFont(host);
 			const value = onRequest.args[0][1]();
-			expect(value).to.eql({family: 'comic sans', size: '20pt'});
+			expect(value).to.eql({
+				dyslexic: false,
+				family: 'comic sans',
+				size: '20pt',
+				visualRedesign: false
+			});
+		});
+
+		it('should return visual redesign if class is present', () => {
+			global.document.body.className = 'visual-redesign';
+			hostSyncFont(host);
+			const value = onRequest.args[0][1]();
+			expect(value).to.eql({
+				dyslexic: false,
+				family: 'comic sans',
+				size: '20pt',
+				visualRedesign: true
+			});
+		});
+
+		it('should return dyslexic if class is present', () => {
+			global.document.body.className = 'vui-dyslexic';
+			hostSyncFont(host);
+			const value = onRequest.args[0][1]();
+			expect(value).to.eql({
+				dyslexic: true,
+				family: 'comic sans',
+				size: '20pt',
+				visualRedesign: false
+			});
+		});
+
+		it('should return dyslexic if font matches', () => {
+			fontFamily = 'Open Dyslexic';
+			hostSyncFont(host);
+			const value = onRequest.args[0][1]();
+			expect(value).to.eql({
+				dyslexic: true,
+				family: 'Open Dyslexic',
+				size: '20pt',
+				visualRedesign: false
+			});
 		});
 
 	});
