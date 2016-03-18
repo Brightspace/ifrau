5601058c74270dd914e5da2de7722dc621f2de40
Martin Bayly <Martin.Bayly@D2L.com>
2016-03-18 12:27:47 -0700

    update custom request type error to use inherits

--- src/port/request-type-error.js
+++ src/port/request-type-error.js
@@ -1,11 +1,10 @@
 'use strict';
+var inherits = require('inherits');
 
 function RequestTypeError(requestType) {
 	this.name = 'RequestTypeError';
 	this.message = requestType ? 'No onRequest handler for type "' + requestType + '"' : 'No handler defined for request';
 }
-
-RequestTypeError.prototype = Object.create(Error.prototype);
-RequestTypeError.prototype.constructor = RequestTypeError;
+inherits(RequestTypeError, Error);
 
 module.exports = RequestTypeError;
