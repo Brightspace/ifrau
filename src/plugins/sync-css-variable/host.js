99a36fc9470ba24b91075c1800f37a70ae1c5a58
Edward Gao <Edward.Gao@D2L.com>
2018-03-27 16:23:56 -0400

    US95607-fix the format

--- src/plugins/sync-css-variable/host.js
+++ src/plugins/sync-css-variable/host.js
@@ -2,10 +2,10 @@
 
 module.exports = function hostSyncCssVariable(host) {
 	host.onRequest('css-variable', function() {
-		var elem  = document.getElementById("d2l-branding-vars");
-		if (elem){
-			var data = elem.getAttribute("data-css-vars");
-			var cssVariables = JSON.parse(data);		
+		var elem  = document.getElementById('d2l-branding-vars');
+		if (elem) {
+			var data = elem.getAttribute('data-css-vars');
+			var cssVariables = JSON.parse(data);
 			return cssVariables;
 		}
 		return {};
