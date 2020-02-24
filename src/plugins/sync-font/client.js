6e15353ed02447ba1df81ed3340ed06995b9e7d6
Mohamed Abo El Soud <mohamed.aboelsoud@d2l.com>
2020-02-24 14:47:17 -0500

    removed outdated visualRedesign

--- src/plugins/sync-font/client.js
+++ src/plugins/sync-font/client.js
@@ -2,23 +2,6 @@
 
 module.exports = function clientSyncFont(client) {
 	return client.request('font').then(function(font) {
-		var size = font.size;
-		if (!font.visualRedesign) {
-			switch (font.size) {
-				case '11px':
-					size = '18px';
-					break;
-				case '17px':
-					size = '22px';
-					break;
-				case '26px':
-					size = '24px';
-					break;
-				default:
-					size = '20px';
-					break;
-			}
-		}
-		document.documentElement.style.fontSize = size;
+		document.documentElement.style.fontSize = font.size;
 	});
 };
