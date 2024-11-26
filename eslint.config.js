4f34fa5c0f6b91509d9876b10fbfbd62ab6ffd45
GZolla <43836485+GZolla@users.noreply.github.com>
2024-11-26 10:14:17 -0800

    fix: Use new shared configs

--- eslint.config.js
+++ eslint.config.js
@@ -1,31 +1,23 @@
-import { fileURLToPath } from 'node:url';
-import { FlatCompat } from '@eslint/eslintrc';
+import { browserConfig, setDirectoryConfigs, testingConfig } from 'eslint-config-brightspace';
 import globals from 'globals';
-import js from '@eslint/js';
-import path from 'node:path';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({
-	baseDirectory: __dirname,
-	recommendedConfig: js.configs.recommended,
-	allConfig: js.configs.all
-});
-export default [...compat.extends('brightspace/browser-config'), {
-	languageOptions: {
-		globals: {
-			...globals.node,
+export default [
+	...setDirectoryConfigs(
+		browserConfig,
+		{
+			test: testingConfig
+		}
+	), {
+		languageOptions: {
+			globals: {
+				...globals.node,
+			},
+		},
+
+		rules: {
+			'no-empty': ['error', {
+				allowEmptyCatch: true,
+			}],
 		},
 	},
-
-	rules: {
-		'no-empty': ['error', {
-			allowEmptyCatch: true,
-		}],
-	},
-},
-...compat.extends('brightspace/testing-config').map(c => ({
-	...c,
-	files: ['test/**/*']
-}))
 ];
